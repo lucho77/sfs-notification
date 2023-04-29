7530d940846a8f92dca015cb4a13d2d88cc83e20
luciano <luoronio@gmail.com>
2023-04-28 23:55:07 -0300

    websocket

--- src/main/java/sfs/notification/notification/NotificationSocket.java
+++ src/main/java/sfs/notification/notification/NotificationSocket.java
@@ -1,30 +1,36 @@
 package sfs.notification.notification;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import javax.enterprise.context.ApplicationScoped;
+import javax.websocket.EndpointConfig;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
-import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
 import sfs.notification.dtos.LoginDTO;
 
-@ServerEndpoint("/sfs-websocket")
+@ServerEndpoint(value = "/sfs-websocket" )
 @ApplicationScoped
 public class NotificationSocket {
-
-    @OnOpen
-    public void onOpen(Session session, LoginDTO loginDTO) {
-    }
+	Map<LoginDTO, Session> sessions = new ConcurrentHashMap<>(); 
+    
+	
+	@OnOpen
+    public void onOpen(Session session, EndpointConfig config) {
+		System.out.println("a ver");
+	}
 
     @OnClose
-    public void onClose(Session session, LoginDTO loginDTO) {
+    public void onClose(Session session) {
     }
-    public void onError(Session session, @PathParam("name") String name, Throwable throwable) {
+    public void onError(Session session,Throwable throwable) {
     }
 
     @OnMessage
-    public void onMessage(String message, LoginDTO loginDTO) {
+    public void onMessage(Session session,String message) {
     }
 }
